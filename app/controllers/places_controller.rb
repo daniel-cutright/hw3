7edd19152a1e9eaaae858afd77de42e2488a71e1
daniel-cutright <daniel.cutright@kellogg.northwestern.edu>
2026-02-20 23:35:57 +0000

    Made updates to views and controllers

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,4 +4,10 @@ class PlacesController < ApplicationController
     @places = Place.all
   end
 
+  def show
+    @id = params["id"]
+    @place = Place.find_by({"id" => @id})
+    @entries = Entry.where({"place_id" => @place["id"]})
+  end
+
 end
